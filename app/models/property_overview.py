01d2d92c563a08383efef40addd782c08d53a423
Takudai Kawasaki tkawasaki <takudai0717@gmail.com>
2025-02-02 19:34:01 +0900

    Refactor CommonOverview model to use English field names and update example data

--- app/models/property_overview.py
+++ app/models/property_overview.py
@@ -6,32 +6,32 @@ from pydantic import BaseModel, Field
 from app.models.id import PyObjectId
 
 # {
-#     "_id": {"$oid": "6790f0aa68873b21a872d2a8"},
-#     "販売スケジュール": "-",
-#     "イベント情報": "-",
-#     "販売戸数": "1戸",
-#     "最多価格帯": "-",
-#     "価格": "7480万円",
-#     "管理費": "1万8760円／月（委託(通勤)）",
-#     "修繕積立金": "1万9320円／月",
-#     "修繕積立基金": "-",
-#     "諸費用": "-",
-#     "間取り": "3DK+S（納戸）",
-#     "専有面積": "74.17m",
-#     "その他面積": "（壁芯）",
-#     "引渡可能時期": "相談",
-#     "完成時期(築年月)": "1969年7月",
-#     "所在階": "5階",
-#     "向き": "南東",
-#     "エネルギー消費性能": "-",
-#     "断熱性能": "-",
-#     "目安光熱費": "-",
-#     "リフォーム": "-",
-#     "その他制限事項": "-",
-#     "その他概要・特記事項": "担当者：神鳥 直人",
-#     "created_at": {"$date": "2025-01-22T22:20:42.787Z"},
-#     "updated_at": {"$date": "2025-01-22T22:20:42.787Z"},
-#     "property_id": {"$oid": "6790f0aa68873b21a872d2a7"},
+#     "_id": {"$oid": "679f48bb824469aa5fc15392"},
+#     "sales_schedule": "-",
+#     "event_information": "-",
+#     "number_of_units_for_sale": "1戸",
+#     "highest_price_range": "-",
+#     "price": "1億4700万円",
+#     "maintenance_fee": "8906円／月（自主管理(管理員なし)）",
+#     "repair_reserve_fund": "2万4618円／月",
+#     "first_repair_reserve_fund": "-",
+#     "other_expenses": "-",
+#     "floor_plan": "2LDK",
+#     "area": "92.36m",
+#     "other_area": "（27.93坪）（壁芯）",
+#     "delivery_time": "相談",
+#     "completion_time": "2009年12月",
+#     "floor": "2階",
+#     "direction": "北",
+#     "energy_consumption_performance": "-",
+#     "insulation_performance": "-",
+#     "estimated_utility_cost": "-",
+#     "renovation": "-",
+#     "other_restrictions": "※所在階：2階・3階 ※上記バルコニー向きはルーフテラスの向きになります。 ●ルーフテラス面積／約40.31平米（約12.19坪）：使用料無償 （上記ルーフテラス面積については計画概要に基づく面積です。） ●専用駐輪場／有：使用料無償（令和6年5月11日現在） ●ペットの飼育については別途ペット使用細則を遵守していただきます。 ●間取図の畳数表記（J）は1畳＝1.62平米で換算した約表示です。 ●略号凡例：Sto.／収納、R／冷蔵庫置場",
+#     "other_overview_and_special_notes": "担当者：大野博史",
+#     "created_at": {"$date": "2025-02-02T19:28:11.628Z"},
+#     "updated_at": {"$date": "2025-02-02T19:28:11.628Z"},
+#     "property_id": {"$oid": "679f48bb824469aa5fc15391"},
 # }
 
 
@@ -39,50 +39,31 @@ class PropertyOverview(BaseModel):
     id: Optional[PyObjectId] = Field(
         alias="_id", default=None, title="the id of the property"
     )
-    販売スケジュール: str = Field(..., title="the sales schedule of the property")
-    イベント情報: str = Field(..., title="the event information of the property")
-    販売戸数: str = Field(..., title="the number of units for sale")
-    最多価格帯: str = Field(..., title="the highest price range")
-    価格: str = Field(..., title="the price of the property")
-    管理費: str = Field(..., title="the management fee of the property")
-    # The difference between 修繕積立金 and 修繕積立基金 is explained in https://suumo.jp/article/oyakudachi/oyaku/ms_shinchiku/ms_money/ms_shuzentsumitatekikin/
-    # 修繕積立金: 建物の外壁やエントランス、屋上などの共用部分を維持し、修繕するために行われる「大規模修繕」などに必要な資金
-    # 修繕積立基金: 第一回の大規模修繕工事に充てるためのお金
-    修繕積立金: str = Field(
-        ...,
-        title="the fund required for “major repairs” and other necessary repairs to maintain and repair common areas such as building exterior walls, entrances, rooftops, etc.",
+    sales_schedule: str = Field(..., title="the sales schedule")
+    event_information: str = Field(..., title="the event information")
+    number_of_units_for_sale: str = Field(..., title="the number of units for sale")
+    highest_price_range: str = Field(..., title="the highest price range")
+    price: str = Field(..., title="the price")
+    maintenance_fee: str = Field(..., title="the maintenance fee")
+    repair_reserve_fund: str = Field(..., title="the repair reserve fund")
+    first_repair_reserve_fund: str = Field(..., title="the first repair reserve fund")
+    other_expenses: str = Field(..., title="the other expenses")
+    floor_plan: str = Field(..., title="the floor plan")
+    area: str = Field(..., title="the area")
+    other_area: str = Field(..., title="the other area")
+    delivery_time: str = Field(..., title="the delivery time")
+    completion_time: str = Field(..., title="the completion time")
+    floor: str = Field(..., title="the floor")
+    direction: str = Field(..., title="the direction")
+    energy_consumption_performance: str = Field(
+        ..., title="the energy consumption performance"
     )
-    修繕積立基金: str = Field(
-        ..., title="the money to be used for the first major repair work"
-    )
-    諸費用: str = Field(..., title="the other expenses of the property")
-    間取り: str = Field(..., title="the floor plan of the property")
-    専有面積: str = Field(..., title="the area of the property")
-    その他面積: str = Field(
-        ..., title="the other area of the property other than 占有面積"
-    )
-    引渡可能時期: str = Field(..., title="the rough delivery time of the property")
-    # This field can be optional
-    完成時期_築年月: Optional[str] = Field(
-        None, title="the completion time of the property"
-    )
-    所在階: str = Field(..., title="the floor of the property")
-    向き: str = Field(..., title="the direction of the property")
-    エネルギー消費性能: str = Field(
-        ..., title="the energy consumption performance of the property"
-    )
-    断熱性能: str = Field(..., title="the insulation performance of the property")
-    目安光熱費: str = Field(
-        ..., title="the estimated lighting and heating cost of the property"
-    )
-    リフォーム: str = Field(..., title="the renovation information of the property")
-    # This field can be optional
-    その他制限事項: Optional[str] = Field(
-        None, title="the other restrictions of the property"
-    )
-    # This field can be optional\
-    その他概要_特記事項: Optional[str] = Field(
-        None, title="the other overview and special notes of the property"
+    insulation_performance: str = Field(..., title="the insulation performance")
+    estimated_utility_cost: str = Field(..., title="the estimated utility cost")
+    renovation: str = Field(..., title="the renovation")
+    other_restrictions: str = Field(..., title="the other restrictions")
+    other_overview_and_special_notes: str = Field(
+        ..., title="the other overview and special notes"
     )
     created_at: datetime = Field(
         ..., title="the date and time the property was created"
@@ -95,35 +76,37 @@ class PropertyOverview(BaseModel):
     class Config:
         json_schema_extra = {
             "example": {
-                "販売スケジュール": "-",
-                "イベント情報": "-",
-                "販売戸数": "1戸",
-                "最多価格帯": "-",
-                "価格": "7480万円",
-                "管理費": "1万8760円／月（委託(通勤)）",
-                "修繕積立金": "1万9320円／月",
-                "修繕積立基金": "-",
-                "諸費用": "-",
-                "間取り": "3DK+S（納戸）",
-                "専有面積": "74.17m",
-                "その他面積": "（壁芯）",
-                "引渡可能時期": "相談",
-                "完成時期(築年月)": "1969年7月",
-                "所在階": "5階",
-                "向き": "南東",
-                "エネルギー消費性能": "-",
-                "断熱性能": "-",
-                "目安光熱費": "-",
-                "リフォーム": "-",
-                "その他制限事項": "-",
-                "その他概要・特記事項": "担当者：神鳥 直人",
-                "created_at": "2025-01-22T22:20:42.787Z",
-                "updated_at": "2025-01-22T22:20:42.787Z",
-                "property_id": "6790f0aa68873b21a872d2a7",
+                "id": "679f48bb824469aa5fc15392",
+                "sales_schedule": "-",
+                "event_information": "-",
+                "number_of_units_for_sale": "1戸",
+                "highest_price_range": "-",
+                "price": "1億4700万円",
+                "maintenance_fee": "8906円／月（自主管理(管理員なし)）",
+                "repair_reserve_fund": "2万4618円／月",
+                "first_repair_reserve_fund": "-",
+                "other_expenses": "-",
+                "floor_plan": "2LDK",
+                "area": "92.36m",
+                "other_area": "（27.93坪）（壁芯）",
+                "delivery_time": "相談",
+                "completion_time": "2009年12月",
+                "floor": "2階",
+                "direction": "北",
+                "energy_consumption_performance": "-",
+                "insulation_performance": "-",
+                "estimated_utility_cost": "-",
+                "renovation": "-",
+                "other_restrictions": "※所在階：2階・3階 ※上記バルコニー向きはルーフテラスの向きになります。 ●ルーフテラス面積／約40.31平米（約12.19坪）：使用料無償 （上記ルーフテラス面積については計画概要に基づく面積です。） ●専用駐輪場／有：使用料無償（令和6年5月11日現在） ●ペットの飼育については別途ペット使用細則を遵守していただきます。 ●間取図の畳数表記（J）は1畳＝1.62平米で換算した約表示です。 ●略号凡例：Sto.／収納、R／冷蔵庫置場",
+                "other_overview_and_special_notes": "担当者：大野博史",
+                "created_at": "2025-02-02T19:28:11.628Z",
             }
         }
 
 
+# The difference between 修繕積立金 and 修繕積立基金 is explained in https://suumo.jp/article/oyakudachi/oyaku/ms_shinchiku/ms_money/ms_shuzentsumitatekikin/
+# 修繕積立金: 建物の外壁やエントランス、屋上などの共用部分を維持し、修繕するために行われる「大規模修繕」などに必要な資金
+# 修繕積立基金: 第一回の大規模修繕工事に充てるためのお金
 PROPERTY_OVERVIEW_TRANSLATION_MAP = {
     "販売スケジュール": "sales_schedule",
     "イベント情報": "event_information",
